7a0331e533e3ed799c2c374d86cfefea42d1292f
mrvan79 <nguyenminhvan1998dn@gmail.com>
2023-04-20 18:05:20 +0900

    register 完成

---
+++ src/main/java/com/example/VisaAPI/model/RegisterResponseModel.java
@@ -0,0 +1,13 @@
+package com.example.VisaAPI.model;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class RegisterResponseModel {
+    private Date birthday;
+    public RegisterResponseModel() {
+		
+	}
+}
