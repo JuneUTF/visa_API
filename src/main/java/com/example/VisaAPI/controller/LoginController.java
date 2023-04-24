819f338ad8ce421b7c17b558ee03b71f60d5cd24
mrvan79 <nguyenminhvan1998dn@gmail.com>
2023-04-24 16:08:12 +0900

    完成

--- src/main/java/com/example/VisaAPI/controller/LoginController.java
+++ src/main/java/com/example/VisaAPI/controller/LoginController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,22 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.VisaAPI.model.LoginModel;
 import com.example.VisaAPI.service.LoginService;
 @RestController
+@CrossOrigin
 public class LoginController {
 	@Resource
 	LoginService loginService;
 
 	 @PostMapping("/login")
-	   public  ResponseEntity<List<LoginModel>> findByUsername(@RequestBody LoginModel loginModel) {
-		 System.out.println(loginModel);
+	   public  ResponseEntity<LoginModel> findByUsername(@RequestBody LoginModel loginModel) {
 		 //ユーザー情報をチャックします。
 		 List<LoginModel> user = loginService.findByUsername(loginModel);
 		 	if(user.size()>0) {
 //		 		ユーザー名とパスワードが正しい
-		 		user.get(0).setPassword("ログインできました");
-			 		return ResponseEntity.ok(user);
+		 		loginModel.setRole(user.get(0).getRole());
+		 		loginModel.setPassword("ログインできました");
+		 		loginModel.setStatus("SUCCESS");
+			 		return ResponseEntity.ok(loginModel);
 		 	}else {
 //		 		ユーザー名とパスワードが正しくない
-				return ResponseEntity.ok(user);
+//		 		status 設定
+		 		loginModel.setStatus("DEFEATED");
+				return ResponseEntity.ok(loginModel);
 		 	}
 	    }
 }
